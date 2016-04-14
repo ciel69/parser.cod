2b0dbca640ee350d057eea2f49c46d03fcf2291d
ciel69 <alex453542026@yandex.ru>
2016-04-14 17:45:28 +0300

    v0.6.1

---
+++ application/sql/reviews (3).sql
@@ -0,0 +1,390 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.5
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 14 2016 г., 17:44
+-- Версия сервера: 5.5.48
+-- Версия PHP: 5.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `reviews`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` mediumint(8) unsigned NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `description` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`, `description`) VALUES
+(1, 'admin', 'Administrator'),
+(2, 'members', 'General User');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `link_items`
+--
+
+CREATE TABLE IF NOT EXISTS `link_items` (
+  `id` int(11) NOT NULL,
+  `url` varchar(1000) DEFAULT NULL,
+  `id_parser` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `list_parser`
+--
+
+CREATE TABLE IF NOT EXISTS `list_parser` (
+  `id` int(11) NOT NULL,
+  `name_parser` varchar(145) DEFAULT NULL,
+  `id_source` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `list_parser`
+--
+
+INSERT INTO `list_parser` (`id`, `name_parser`, `id_source`) VALUES
+(17, 'Смартфоны', 7),
+(19, 'Смарт-часы', 6),
+(21, 'Смарт-часы', 8),
+(22, 'Бытовая техника', 9),
+(23, 'Бытовая техника', 10),
+(24, 'Смартфоны', 10),
+(25, NULL, NULL),
+(26, NULL, NULL),
+(27, NULL, NULL),
+(28, NULL, 10),
+(29, 'Компьютеры', 10),
+(30, 'Игровые приставки', 10),
+(31, 'Телевизор', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `login_attempts`
+--
+
+CREATE TABLE IF NOT EXISTS `login_attempts` (
+  `id` int(11) unsigned NOT NULL,
+  `ip_address` varchar(15) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `time` int(11) unsigned DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `migrations`
+--
+
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `version` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `migrations`
+--
+
+INSERT INTO `migrations` (`version`) VALUES
+(0);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `new_parser`
+--
+
+CREATE TABLE IF NOT EXISTS `new_parser` (
+  `id` int(11) NOT NULL,
+  `name` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `properties_parser`
+--
+
+CREATE TABLE IF NOT EXISTS `properties_parser` (
+  `id` int(11) NOT NULL,
+  `class_item` varchar(500) DEFAULT NULL,
+  `name_item` varchar(500) DEFAULT NULL,
+  `id_parser` int(11) DEFAULT NULL,
+  `next_link` varchar(500) DEFAULT NULL,
+  `url` varchar(500) DEFAULT NULL,
+  `code_item` varchar(145) DEFAULT NULL,
+  `exceptions` varchar(10000) DEFAULT NULL,
+  `link_reviews` varchar(500) DEFAULT NULL,
+  `class_review` varchar(145) DEFAULT NULL,
+  `class_page_rev` varchar(145) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `properties_parser`
+--
+
+INSERT INTO `properties_parser` (`id`, `class_item`, `name_item`, `id_parser`, `next_link`, `url`, `code_item`, `exceptions`, `link_reviews`, `class_review`, `class_page_rev`) VALUES
+(1, '', NULL, 19, '', NULL, NULL, NULL, NULL, NULL, NULL),
+(2, '', NULL, 21, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 'class_item', 'name', 23, 'class_page', 'www.site', 'cod', 'это не парсить', 'templates_link', 'class_otsiva', 'next_otsiv_page'),
+(10, 'psp', 'name.psp', 30, 'class_page.psp', 'www.site.psp', 'psp.cod', 'это не парсить.psp', 'templates_link', 'class_otsiva', 'next_otsiv_page'),
+(11, 'items_eldorado', NULL, 31, '', NULL, NULL, NULL, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `source_pars`
+--
+
+CREATE TABLE IF NOT EXISTS `source_pars` (
+  `id` int(11) NOT NULL,
+  `name_source` varchar(145) NOT NULL,
+  `tr_name` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `source_pars`
+--
+
+INSERT INTO `source_pars` (`id`, `name_source`, `tr_name`) VALUES
+(3, 'Эльдорадо', 'Eldorado'),
+(6, 'DNS', 'DNS'),
+(8, 'М-видео', 'M-video'),
+(9, 'Техношое', 'Tehnoshoe'),
+(10, 'Техношок', 'Tehnoshok');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `table_test`
+--
+
+CREATE TABLE IF NOT EXISTS `table_test` (
+  `id` int(11) NOT NULL,
+  `name` varchar(145) NOT NULL,
+  `surname` varchar(145) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `table_test`
+--
+
+INSERT INTO `table_test` (`id`, `name`, `surname`) VALUES
+(1, 'ciel', 'phantomhive');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) unsigned NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `activation_code` varchar(40) DEFAULT NULL,
+  `forgotten_password_code` varchar(40) DEFAULT NULL,
+  `forgotten_password_time` int(11) unsigned DEFAULT NULL,
+  `remember_code` varchar(40) DEFAULT NULL,
+  `created_on` int(11) unsigned NOT NULL,
+  `last_login` int(11) unsigned DEFAULT NULL,
+  `active` tinyint(1) unsigned DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `company` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`) VALUES
+(1, '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@admin.com', '', NULL, NULL, 'Pd8hWl.N.mau3FEiqhocae', 1268889823, 1460614415, 1, 'Admin', 'istrator', 'ADMIN', '0'),
+(2, '127.0.0.1', NULL, '$2y$08$x/AV.GDKCM6F1FoDSdTs/OqBXdQ4cX.QhaV9fEXCpg5YLyI39UgYe', NULL, 'alex453542026@yandex.ru', NULL, NULL, NULL, NULL, 1458722044, 1458737244, 1, 'ciel', 'phantomhive', '0', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users_groups`
+--
+
+CREATE TABLE IF NOT EXISTS `users_groups` (
+  `id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `group_id` mediumint(8) unsigned NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users_groups`
+--
+
+INSERT INTO `users_groups` (`id`, `user_id`, `group_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(6, 2, 2);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `link_items`
+--
+ALTER TABLE `link_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `list_parser`
+--
+ALTER TABLE `list_parser`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `new_parser`
+--
+ALTER TABLE `new_parser`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `properties_parser`
+--
+ALTER TABLE `properties_parser`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `source_pars`
+--
+ALTER TABLE `source_pars`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `source_pars_name_uindex` (`name_source`);
+
+--
+-- Индексы таблицы `table_test`
+--
+ALTER TABLE `table_test`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`),
+  ADD KEY `fk_users_groups_users1_idx` (`user_id`),
+  ADD KEY `fk_users_groups_groups1_idx` (`group_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT для таблицы `link_items`
+--
+ALTER TABLE `link_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `list_parser`
+--
+ALTER TABLE `list_parser`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
+--
+-- AUTO_INCREMENT для таблицы `login_attempts`
+--
+ALTER TABLE `login_attempts`
+  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `new_parser`
+--
+ALTER TABLE `new_parser`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `properties_parser`
+--
+ALTER TABLE `properties_parser`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT для таблицы `source_pars`
+--
+ALTER TABLE `source_pars`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT для таблицы `table_test`
+--
+ALTER TABLE `table_test`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT для таблицы `users_groups`
+--
+ALTER TABLE `users_groups`
+  MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `users_groups`
+--
+ALTER TABLE `users_groups`
+  ADD CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
